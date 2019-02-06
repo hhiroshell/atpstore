d710163f10b1f35002542431b1645f463017ec4d
Hiroshi Hayakawa <hhiroshell@gmail.com>
2019-02-06 21:51:58 +0900

    initial commit.

---
+++ src/main/java/jp/gr/java_conf/hhiroshell/atpstore/AtpDataSource.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2019 Hiroshi Hayakawa <hhiroshell@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.gr.java_conf.hhiroshell.atpstore;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+class AtpDataSource {
+
+    private static final String KEY_DATABASE_USERNAME = "DATABASE_USERNAME";
+
+    private static final String KEY_DATABASE_PASSWORD = "DATABASE_PASSWORD";
+
+    private static final String KEY_DATABASE_SERVICE = "DATABASE_SERVICE";
+
+    private static final String KEY_DATABASE_TNS_ADMIN = "DATABASE_TNS_ADMIN";
+
+    private static final String CONNECTION_FACTORY_CLASS_NAME = "oracle.jdbc.pool.OracleDataSource";
+
+    private static final int INITIAL_POOL_SIZE = 5;
+
+    private static DataSource instance;
+
+    // uninstanciable
+    private AtpDataSource() {}
+
+    static DataSource getInstance() {
+        if (instance == null) {
+            instance = getDataSource();
+        }
+        return instance;
+    }
+
+    /**
+     * データソースを取得する
+     *
+     * @return データソース
+     */
+    private static DataSource getDataSource() {
+        PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+        try {
+            poolDataSource.setConnectionFactoryClassName(CONNECTION_FACTORY_CLASS_NAME);
+            poolDataSource.setUser(System.getenv(KEY_DATABASE_USERNAME));
+            poolDataSource.setPassword(System.getenv(KEY_DATABASE_PASSWORD));
+            poolDataSource.setURL(new StringBuilder()
+                    .append("jdbc:oracle:thin:@")
+                    .append(System.getenv(KEY_DATABASE_SERVICE))
+                    .append("?TNS_ADMIN=")
+                    .append(System.getenv(KEY_DATABASE_TNS_ADMIN))
+                    .toString());
+            poolDataSource.setInitialPoolSize(INITIAL_POOL_SIZE);
+            return poolDataSource;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
---
+++ src/main/java/jp/gr/java_conf/hhiroshell/atpstore/GreetService.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2019 Hiroshi Hayakawa <hhiroshell@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.gr.java_conf.hhiroshell.atpstore;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.sql.DataSource;
+
+import io.helidon.config.Config;
+import io.helidon.metrics.RegistryFactory;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
+import io.helidon.webserver.Service;
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+
+import java.sql.SQLException;
+
+/**
+ * A simple service to greet you. Examples:
+ *
+ * Get default greeting message:
+ * curl -X GET http://localhost:8080/greet
+ *
+ * Get greeting message for Joe:
+ * curl -X GET http://localhost:8080/greet/Joe
+ *
+ * Change greeting
+ * curl -X PUT http://localhost:8080/greet/greeting/Hola
+ *
+ * The message is returned as a JSON object
+ */
+
+public class GreetService implements Service {
+
+    /**
+     * The config value for the key {@code greeting}.
+     */
+    private String greeting;
+
+    GreetService(Config config) {
+        this.greeting = config.get("app.greeting").asString().orElse("Ciao");
+    }
+
+    private final MetricRegistry registry = RegistryFactory.getRegistryFactory().get()
+            .getRegistry(MetricRegistry.Type.APPLICATION);
+    private final Counter accessCtr = registry.counter("accessctr");
+
+    /**
+     * A service registers itself by updating the routine rules.
+     * @param rules the routing rules.
+     */
+    @Override
+    public void update(Routing.Rules rules) {
+        rules
+            .any(this::countAccess)
+            .get("/", this::getDefaultMessageHandler)
+            .get("/{name}", this::getMessageHandler)
+            .put("/greeting/{greeting}", this::updateGreetingHandler);
+    }
+
+    /**
+     * Return a wordly greeting message.
+     * @param request the server request
+     * @param response the server response
+     */
+    private void getDefaultMessageHandler(ServerRequest request, ServerResponse response) {
+        sendResponse(response, "World");
+    }
+
+    /**
+     * Return a greeting message using the name that was provided.
+     * @param request the server request
+     * @param response the server response
+     */
+    private void getMessageHandler(ServerRequest request, ServerResponse response) {
+        String name = request.path().param("name");
+        sendResponse(response, name);
+    }
+
+    private void sendResponse(ServerResponse response, String name) {
+        DataSource dataSource = AtpDataSource.getInstance();
+        try {
+            dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String msg = String.format("%s %s!", greeting, name);
+
+        JsonObject returnObject = Json.createObjectBuilder()
+                .add("message", msg)
+                .build();
+        response.send(returnObject);
+    }
+
+    /**
+     * Set the greeting to use in future messages.
+     * @param request the server request
+     * @param response the server response
+     */
+    private void updateGreetingHandler(ServerRequest request, ServerResponse response) {
+        greeting = request.path().param("greeting");
+
+        JsonObject returnObject = Json.createObjectBuilder()
+                .add("greeting", greeting)
+                .build();
+        response.send(returnObject);
+    }
+
+    private void countAccess(ServerRequest request, ServerResponse response) {
+        accessCtr.inc();
+        request.next();
+    }
+
+}
