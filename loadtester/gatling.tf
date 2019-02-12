b68d4f5c8d36ba1d76854654211ab01a4e685882
Hiroshi Hayakawa <hhiroshell@gmail.com>
2019-02-12 14:53:13 +0900

    add loadtester and related k8s resources

--- loadtester/gatling.tf
+++ loadtester/gatling.tf
@@ -5,7 +5,7 @@ resource "oci_core_instance" "gatling" {
     shape               = "${var.core_instance_shape}"
 
     create_vnic_details {
-        subnet_id        = "${var.core_instance_subnet_ocid}"
+        subnet_id = "${var.core_instance_subnet_ocid}"
     }
 
     source_details {
@@ -24,9 +24,40 @@ resource "oci_core_instance" "gatling" {
         private_key = "${file(var.core_instance_ssh_private_key_file)}"
     }
 
-    provisioner "remote-exec" {
+    provisioner "remote-exec" "install jdk & gatling" {
         inline = [
-            "echo 'This instance was provisioned by Terraform.' | sudo tee /etc/motd",
+            #"sudo yum -y update",
+            "sudo yum -y install java-1.8.0-openjdk",
+            "wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.0.1.1/gatling-charts-highcharts-bundle-3.0.1.1-bundle.zip -O /tmp/gatling-charts-highcharts-bundle-3.0.1.1-bundle.zip",
+            "mkdir ~/gatling",
+            "unzip /tmp/gatling-charts-highcharts-bundle-3.0.1.1-bundle.zip -d ~/gatling",
+        ]
+    }
+
+    provisioner "remote-exec" "preparation for copying files" {
+        inline = [
+            "mkdir ~/gatling/gatling-charts-highcharts-bundle-3.0.1.1/user-files/simulations/atpstore",
+        ]
+    }
+
+    provisioner "file" {
+        source = "./gatling-home/simulations/atpstore/atpstoreRead.scala"
+        destination = "/home/opc/gatling/gatling-charts-highcharts-bundle-3.0.1.1/user-files/simulations/atpstore/atpstoreRead.scala"
+    }
+
+    provisioner "file" {
+        source = "./gatling-home/run.sh"
+        destination = "/home/opc/gatling/gatling-charts-highcharts-bundle-3.0.1.1/run.sh"
+    }
+
+    provisioner "file" {
+        source = "./gatling-home/endpoints.txt"
+        destination = "/home/opc/gatling/gatling-charts-highcharts-bundle-3.0.1.1/endpoints.txt"
+    }
+
+    provisioner "remote-exec" "change file permission" {
+        inline = [
+            "chmod +x /home/opc/gatling/gatling-charts-highcharts-bundle-3.0.1.1/run.sh",
         ]
     }
 
